2ed2a68399f98886b01bcbc475b1772d1f423a71
Maciek P <maciek_p@interia.pl>
2025-03-03 22:08:24 +0100

    Python Basics Exercises-Scopes

---
+++ scope-exercises/global_scope.py
@@ -0,0 +1,11 @@
+def print_total():
+    def inner_print_total():
+        total = 7
+        print(f"From inner function: {total=}")
+
+    inner_print_total()
+    print(f"From function: {total=}")
+
+total = 5 
+print_total()
+print(f"From global: {total=}")
---
+++ scope-exercises/local_scope.py
@@ -0,0 +1,8 @@
+def print_total():
+    total = 0
+    print(f"From function: {total=}")
+
+print_total()
+total = 10
+print(f"{total=}")
+
